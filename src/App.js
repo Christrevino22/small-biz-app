266c07f4bc6d03f5568d9b2c63c70e131f02cbfe
Christopher Trevino <chrisdadef@gmail.com>
2021-02-18 13:40:25 -0600

    updated a few things internet it working so so

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
+import React, {useState} from 'react';
 
 
 
 
 
+//debating how i should divide and conquer
 
 function App() {
   // const [] = usestate()
   return (
     <div className="App">
-      <h1>HELLO WORLD!</h1>
+      <h1>HELLO WORLD!!!</h1>
       <select>
         <option>MR </option>
         <option>MISS</option>
         <option>MRS</option>
-
+        <option>WTF</option>
       </select>
     </div>
   );
